417e8b375d74ad2dd70fb7fc2764f40f2d2a164a
fengxueyijiu <473952802@qq.com>
2017-06-15 15:53:42 +0800

    i

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports={
+  appId:'wxedf8dcacbf8a603b',
+  appSecret:'cccaf801c87d3dea8830ddb7149ca7b3',
+  jwtSecret:'ffgff'
+}
--- index.js
+++ index.js
@@ -5,10 +5,17 @@ let bodyParser = require('body-parser');
 let morgan = require('morgan');
 let config = require('./config')
 let axios = require('axios');
+let jwt = require('jsonwebtoken');
 
 mongoose.connect('mongodb://localhost:27017/weapp-server');
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
+let generateToken=function(user) {
+  return jwt.sign(user, config.jwtSecret, {
+    expiresIn: 7200
+  });
+}
 app.post('/login',(req,res) => {
   // const queryString = `appid=${config.appId}&secret=${config.appSecret}&hs_cide=${req.body.code}&grant_type=authorization_code`
   // const wxAPI = `https://api.weixin.qq.com/sns/jscode2session?${queryString}`;
@@ -18,7 +25,7 @@ app.post('/login',(req,res) => {
        .then(response =>{
          console.log(response.data);
          res.json({
-           message:'success!'
+           token: generateToken({openid: response.data.openid})
          })
        })
        .catch(error => {
